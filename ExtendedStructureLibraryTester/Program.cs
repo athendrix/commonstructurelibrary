085b767df88c282f780235d9939de53e18bc6cee
YendisFish <brintonsid@gmail.com>
2021-10-10 16:33:08 -0700

    [Dysfunctional] Added ESL tester

---
+++ ExtendedStructureLibraryTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExtendedStructureLibraryTester
+{
+    public class Program
+    {
+        //main function of ESL tester
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tester Init!");
+
+            //add logic
+        }
+    }
+}
---
+++ ExtendedStructureLibraryTester/Testers/Passwords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//dysfunctional
+using CSL;
+
+namespace ExtendedStructureLibraryTester.Testers
+{
+    public class Passwords
+    {
+    }
+}
