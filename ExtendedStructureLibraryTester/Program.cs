2d5a9e157efbaab0312a37baab01ad5961a13f62
YendisFish <brintonsid@gmail.com>
2021-10-14 16:16:33 -0700

    [Dysfunctional] Added test tasks in ESLTester

--- ExtendedStructureLibraryTester/Program.cs
+++ ExtendedStructureLibraryTester/Program.cs
@@ -1,16 +1,34 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ExtendedStructureLibraryTester
 {
-    public class Program
+    class Program
     {
+        private static Task testClient()
+        {
+            Task main = new Task(() => CSL.Sockets.Clients.TCPClient("25892", "hello", "127.0.0.1"));
+
+            return main;
+        }
+        private static Task testServer()
+        {
+            Task main = new Task(() => CSL.Sockets.Servers.TCPServer(new CSL.Sockets.ServerInfo("127.0.0.1", "25892", null)));
+
+            return main;
+        }
+        
         //main function of ESL tester
         static void Main(string[] args)
         {
             Console.WriteLine("Tester Init!");
 
             //add logic
-            Console.WriteLine(Testers.Passwords.ThreeLetterPassb);
+            Task Server = testServer();
+            Task Client = testClient();
+
+            Server.Start();
+            Client.Start();
         }
     }
 }
