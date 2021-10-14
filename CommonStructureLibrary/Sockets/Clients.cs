9f501548a9be0f47f880f8a66a4303887bd47002
YendisFish <brintonsid@gmail.com>
2021-10-14 15:58:04 -0700

    [Untested, added socket functionality back]

---
+++ CommonStructureLibrary/Sockets/Clients.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CSL.Sockets
+{
+    public class Clients
+    {
+        public static void TCPClient(string? port = null, string? data = null, string? target = "localhost")
+        {
+            TcpClient client = new TcpClient();
+
+            Console.WriteLine($"Connecting to {target}:{port}");
+
+            try
+            {
+                client.Connect(target, Convert.ToInt32(port));
+
+                Console.WriteLine("Connected!");
+
+                if (data != null)
+                {
+                    Console.WriteLine("data is: " + data);
+
+                    Stream stream = client.GetStream();
+
+                    ASCIIEncoding encoding = new ASCIIEncoding();
+                    byte[] x = encoding.GetBytes(data);
+
+                    Console.WriteLine("Sending data...");
+
+                    stream.Write(x, 0, x.Length);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to connect to {target}:{port}");
+            }
+
+
+        }
+    }
+}
---
+++ CommonStructureLibrary/Sockets/Servers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CSL.Sockets
+{
+    public record ServerInfo(string target, string port, string? data, string? name = null);
+
+
+    public class Servers
+    {
+        public static void TCPServer(ServerInfo args)
+        {
+            IPAddress ip = IPAddress.Parse(args.target);
+
+            try
+            {
+                TcpListener Listener = new TcpListener(ip, Convert.ToInt32(args.port));
+
+                Listener.Start();
+
+                Console.WriteLine($"({args.target}) | Listening on {args.port}");
+                Console.WriteLine($"Local endpoint is {Listener.LocalEndpoint}");
+
+                Console.WriteLine("Waiting for connection...");
+
+                Socket socket = Listener.AcceptSocket();
+                Console.WriteLine($"New connection ({socket.RemoteEndPoint})");
+
+                byte[] x = new byte[100];
+                int y = socket.Receive(x);
+                Console.WriteLine("Incoming data...");
+
+                for (int m = 0; m < y; m++)
+                {
+                    Console.Write(Convert.ToChar(x[m]));
+                }
+                Console.WriteLine("");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to bind to port " + args.port + ". Maybe it is being used by another process?");
+                Console.WriteLine("EXCEPTION: " + ex.Message);
+            }
+        }
+    }
+}
