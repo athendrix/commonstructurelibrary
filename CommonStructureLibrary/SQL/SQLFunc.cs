e4e5a9733375256acf9391d6da34b09feba892e0
Andrew Hendrix <ahendrix@pentaracorp.com>
2023-05-10 10:20:09 -0600

    Fixed SQLFuncs and updated Project Versions

--- CommonStructureLibrary/SQL/SQLFunc.cs
+++ CommonStructureLibrary/SQL/SQLFunc.cs
@@ -5,29 +5,19 @@ using System.Threading.Tasks;
 
 namespace CSL.SQL
 {
-    public abstract class SQLFunc
+    public static class SQLFunc
     {
-        protected SQLFunc(string SQLCode) => this.SQLCode = SQLCode;
-        protected readonly string SQLCode = "";
-    }
-    public class SQLFunc<Input, Output> : SQLFunc where Input : CSLRecord<Input> where Output : CSLRecord<Output>
-    {
-        public SQLFunc(string SQLCode) : base(SQLCode) { }
-        public async Task<AutoClosingEnumerable<Output>> Run(SQLDB sql, Input input) => (await sql.ExecuteReader(SQLCode, sql.ConvertToFriendlyParameters(input.ToArray()))).ReadRecords<Output>(sql);
-    }
-    public class SQLFunc<Output> : SQLFunc where Output : CSLRecord<Output>
-    {
-        public SQLFunc(string SQLCode) : base(SQLCode) { }
-        public async Task<AutoClosingEnumerable<Output>> Run(SQLDB sql) => (await sql.ExecuteReader(SQLCode)).ReadRecords<Output>(sql);
-    }
-    public class SQLAction<Input> : SQLFunc where Input : CSLRecord<Input>
-    {
-        public SQLAction(string SQLCode) : base(SQLCode) { }
-        public Task<int> Run(SQLDB sql, Input input) => sql.ExecuteNonQuery(SQLCode, sql.ConvertToFriendlyParameters(input.ToArray()));
-    }
-    public class SQLAction : SQLFunc
-    {
-        public SQLAction(string SQLCode) : base(SQLCode) { }
-        public Task<int> Run(SQLDB sql) => sql.ExecuteNonQuery(SQLCode);
+        public static Func<SQLDB, Input, Task<AutoClosingEnumerable<Output>>> GetFunc<Input, Output>(string SQLCode)
+            where Input : CSLRecord<Input> where Output : CSLRecord<Output> =>
+            async (SQLDB sql, Input input) => (await sql.ExecuteReader(SQLCode, sql.ConvertToFriendlyParameters(input.ToArray()))).ReadRecords<Output>(sql);
+
+        public static Func<SQLDB, Task<AutoClosingEnumerable<Output>>> GetFunc<Output>(string SQLCode)
+            where Output : CSLRecord<Output> =>
+            async (SQLDB sql) => (await sql.ExecuteReader(SQLCode)).ReadRecords<Output>(sql);
+
+        public static Func<SQLDB, Input, Task<int>> GetAction<Input>(string SQLCode)
+            where Input : CSLRecord<Input> =>
+            (SQLDB sql, Input input) => sql.ExecuteNonQuery(SQLCode, sql.ConvertToFriendlyParameters(input.ToArray()));
+        public static Func<SQLDB, Task<int>> GetAction(string SQLCode) => (SQLDB sql) => sql.ExecuteNonQuery(SQLCode);
     }
 }
